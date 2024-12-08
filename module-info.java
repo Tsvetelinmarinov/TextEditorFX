92460f8046c9372800d5fd22d4a7d43ba5daff21
Tsvetelin Marinov <cvetelinmarinov566@gmail.com>
2024-12-08 19:51:14 +0200

    Update 08/12/2024 -- module-info.java
    Options added:
     -- Application name in the top
    
     Bugs fixed:
      -- fixed spacing in the module

--- module-info.java
+++ module-info.java
@@ -1,11 +1,14 @@
+
 /**
- * 
+ * TextEditor++
  */
 module TextEditorFX {
+	
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires java.desktop;
 	requires javafx.graphics;
+	requires javafx.base;
 	
 	opens appdata to javafx.graphics, javafx.fxml;
 }
