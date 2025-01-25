4fbc1e96be5dc2b4902b7afd09810e7e9281eeab
Tsvetelin Marinov <Cvetelinmarinov566@gmail.com>
2025-01-25 15:50:29 +0200

    Update 25/01/2025 part2 - Main
    Options added:
     - Few new components like labels,combo boxes and buttons

--- src/main/java/appdata/Settings.java
+++ src/main/java/appdata/Settings.java
@@ -4,20 +4,21 @@
 
 package appdata;
 
+import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-
 import java.awt.*;
+
 
 /**
  *
@@ -41,6 +42,15 @@ public class Settings {
     private ComboBox<String> fontBox = new ComboBox<String>(fonts);
     private SpinnerValueFactory<Integer> sizeSpin = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 50, 20);
     private Spinner<Integer> sizeSpinner = new Spinner<Integer>(sizeSpin);
+    private ComboBox<String> styleBox = new ComboBox<String>(stylesList);
+    private ColorPicker fontColorChooser = new ColorPicker();
+    private Button resetDefaultFontSettings = new Button("Reset defaults");
+    private Label theme = new Label("theme");
+    private String[] themes = {"classic", "dark", "darker", "gray"};
+    private ObservableList<String> themesList = FXCollections.observableArrayList(themes);
+    private Button manualThemeConfiguration = new Button("manual appearance configuration");
+
+
 
 
 
@@ -62,6 +72,19 @@ public class Settings {
         settingsRoot.getChildren().add(fontSection);
 
 
+        fontFamily.setFont(Font.font("System", FontWeight.NORMAL, FontPosture.REGULAR, 16));
+        fontFamily.setPrefSize(70,20);
+        fontFamily.setLayoutX(45);
+        fontFamily.setLayoutY(140);
+        settingsRoot.getChildren().add(fontFamily);
+
+
+        fontBox.setPrefSize(200,25);
+        fontBox.setLayoutX(90);
+        fontBox.setLayoutY(140);
+        settingsRoot.getChildren().add(fontBox);
+        fontBox.setOnAction(event -> changeEditorsFont(fontBox, null));
+
 
         settingsStage.setTitle("Appearance settings");
         settingsStage.setScene(settingsUI);
@@ -69,5 +92,12 @@ public class Settings {
         settingsStage.show();
     }
 
+
+
+    //Смени шрифта на едитора
+    private void changeEditorsFont(ComboBox<String> fontsBox, TextArea textArea) {
+        String pickedFont = fontBox.getSelectionModel().getSelectedItem();
+        textArea.setFont(Font.font(pickedFont));
+    }
 
 }
